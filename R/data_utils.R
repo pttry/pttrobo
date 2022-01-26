e2663532e036150ebeb63d4b4b63e47cd5d8137d
Janne Huovari <janne.huovari@ptt.fi>
2022-01-26 13:57:55 +0200

    pttrobo_print_filter_recode

--- R/data_utils.R
+++ R/data_utils.R
@@ -7,6 +7,7 @@
 #' @export
 #' @examples
 #'   pttrobo_print_filter(x = "luke/02_Maatalous/06_Talous/02_Maataloustuotteiden_tuottajahinnat/08_Tuottajahinnat_Vilja_rypsi_rapsi_v.px", conc = FALSE)
+#'   pttrobo_print_filter_recode(x = "luke/02_Maatalous/06_Talous/02_Maataloustuotteiden_tuottajahinnat/08_Tuottajahinnat_Vilja_rypsi_rapsi_v.px", conc = FALSE)
 
 pttrobo_print_filter <- function(x, conc = TRUE){
   if (!inherits(x, "robonomist_data")){
@@ -31,5 +32,32 @@ pttrobo_print_filter <- function(x, conc = TRUE){
     )
  cat(out)
  if (conc) cat(out, file = "clipboard-128")
+
+}
+
+#' @describeIn pttrobo_print_filter
+pttrobo_print_filter_recode <- function(x, conc = TRUE){
+  if (!inherits(x, "robonomist_data")){
+    x <- ptt_data_robo_l(x)
+  }
+
+  y <- lapply(x, function(x) {
+    if (is.character(x) | is.factor(x)) {
+      unique(x)
+    }})
+
+  y <- y[!unlist(lapply(y, is.null))]
+
+
+
+  out <- paste0(
+    "filter_recode(\n  ",
+    paste0(purrr::imap(y, ~paste0(.y," = c(\"", paste0(as.character(.x), collapse = "\", \""), "\")")), collapse = ",\n  "),
+    "\n  )"
+
+
+  )
+  cat(out)
+  if (conc) cat(out, file = "clipboard-128")
 
 }
