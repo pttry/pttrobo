29ca6baee57683bace629295a57872fbcb3221c7
Janne Huovari <janne.huovari@ptt.fi>
2022-02-01 11:38:00 +0200

    plot_trends

--- R/plotting.R
+++ R/plotting.R
@@ -38,3 +38,43 @@ aplot_lines <- function(dat, x = time, y = value,
                    title = title, subtitle = subtitle, lahde = caption,
                    yksikko = NULL, ...)
 }
+
+#' @describeIn aplot_lines
+#' @export
+#' @examples
+#' ptt_data_robo_l("StatFin/kan/ntp/statfin_ntp_pxt_132h.px") |>
+#' dplyr::filter(
+#'     taloustoimi %in% c("P7R Tavaroiden ja palvelujen tuonti, tulona",
+#'     "P6K Tavaroiden ja palvelujen vienti, menona"),
+#'     tiedot == c("Alkuperäinen sarja, viitevuosi 2015, miljoonaa euroa")) |>
+#'     aplot_trends(colour = taloustoimi,
+#'                 title = "Vienti ja tuonti",
+#'                 subtititle = "%, volyymin muutos",
+#'                 source = "Tilastokeskus")
+
+aplot_trends <- function(dat, x = time, y = value,
+                         colour = tiedot, size = NULL,
+                         title = NULL, subtitle = NULL,
+                         source = NULL,
+                         caption = NULL, ...){
+  piirtaja <- ptt_plot()
+
+  dat <-
+    dat |>
+    droplevels() |>
+    mutate(alk = {{y}}) |>
+    group_by({{colour}}) |>
+    mutate(value = statfitools::trend_series({{y}}, time)) |>
+    ungroup()
+
+  tiedot_name <- rlang::enquo(colour)
+
+  piirtaja$lines(dat, grouping_variable = {{colour}},
+                 title = title, subtitle = subtitle, lahde = caption,
+                 yksikko = NULL, legendgroup = tiedot_name) |>
+    plotly::add_lines(y = dat$alk,
+                      size = I(0.5),
+                      legendgroup = tiedot_name,
+                      showlegend = FALSE
+    )
+}
