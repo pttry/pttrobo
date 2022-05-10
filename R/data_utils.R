d35d2b910112f95af19456b17972f64b4dc0ae41
Janne Huovari <janne.huovari@ptt.fi>
2022-05-10 14:49:03 +0300

    pttrobo_print_code

--- R/data_utils.R
+++ R/data_utils.R
@@ -1,15 +1,51 @@
+#' Print get code and full filtering for Robonomist id or url
+#'
+#' @examples
+#'   pttrobo_print_code("https://pxweb2.stat.fi/PxWeb/pxweb/fi/StatFin/StatFin__vaerak/statfin_vaerak_pxt_11ra.px/", conc = FALSE)
+#'   pttrobo_print_code("StatFin/vaerak/statfin_vaerak_pxt_11ra.px", conc = FALSE)
+#'
+pttrobo_print_code <-
+  function(x, conc = TRUE){
+
+    check_url <- httr::parse_url(x)
+
+    if (!is.null(check_url$scheme) && check_url$scheme %in% c("http", "https")){
+      id <- robonomistClient::data_search(x)$id
+
+      if (length(id) == 0) stop("Url: ", x, " not found in Robonomist data.")
+      if (length(id) != 1) {
+        message("Several ids found. First will be used\n", id)
+        id <- id[1]
+      }
+
+    } else {
+      id <- x
+    }
+
+    out <- paste0(
+      "ptt_data_robo(", id, ") |>\n  ",
+      pttrobo_print_filter_recode(id, conc = FALSE, print = FALSE)
+
+    )
+    cat(out)
+    if (conc) cat(out, file = "clipboard-128")
+
+  }
+
+
 #' Print full filtering for Robonomist id or data
 #'
 #' In ptt-format
 #'
 #' @param x A Robonomist id or robonomist_data
 #' @param conc A locigal whether to copy in clipboard
+#' @param print A locigal whether to print output (to only return invisibly)
 #' @export
 #' @examples
 #'   pttrobo_print_filter(x = "luke/02_Maatalous/06_Talous/02_Maataloustuotteiden_tuottajahinnat/08_Tuottajahinnat_Vilja_rypsi_rapsi_v.px", conc = FALSE)
 #'   pttrobo_print_filter_recode(x = "luke/02_Maatalous/06_Talous/02_Maataloustuotteiden_tuottajahinnat/08_Tuottajahinnat_Vilja_rypsi_rapsi_v.px", conc = FALSE)
 
-pttrobo_print_filter <- function(x, conc = TRUE){
+pttrobo_print_filter <- function(x, conc = TRUE, print = TRUE){
   if (!inherits(x, "robonomist_data")){
     x <- ptt_data_robo_l(x)
   }
@@ -30,14 +66,16 @@ pttrobo_print_filter <- function(x, conc = TRUE){
 
 
     )
- cat(out)
- if (conc) cat(out, file = "clipboard-128")
+
+  if (print) cat(out)
+  if (conc) cat(out, file = "clipboard-128")
+  invisible(out)
 
 }
 
 #' @describeIn pttrobo_print_filter version for pttdatahaku::filter_recode()
 #' @export
-pttrobo_print_filter_recode <- function(x, conc = TRUE){
+pttrobo_print_filter_recode <- function(x, conc = TRUE, print = TRUE){
   if (!inherits(x, "robonomist_data")){
     x <- ptt_data_robo_l(x)
   }
@@ -52,13 +90,14 @@ pttrobo_print_filter_recode <- function(x, conc = TRUE){
 
 
   out <- paste0(
-    "filter_recode(\n  ",
-    paste0(purrr::imap(y, ~paste0(.y," = c(\"", paste0(as.character(.x), collapse = "\", \""), "\")")), collapse = ",\n  "),
+    "filter_recode(\n    ",
+    paste0(purrr::imap(y, ~paste0(.y," = c(\"", paste0(as.character(.x), collapse = "\", \""), "\")")), collapse = ",\n    "),
     "\n  )"
 
 
   )
-  cat(out)
+  if (print) cat(out)
   if (conc) cat(out, file = "clipboard-128")
+  invisible(out)
 
 }
