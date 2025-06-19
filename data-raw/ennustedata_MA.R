ce6b42b4c8596e6427952e36548197169665e6ca
mavalonen <99964446+mavalonen@users.noreply.github.com>
2025-06-19 14:05:13 +0300

    Update ennustedata_MA.R_taulujen_nimen_muutokset pg_build

--- data-raw/ennustedata_MA.R
+++ data-raw/ennustedata_MA.R
@@ -1,35 +1,26 @@
 
 
-id_ulkomaank <- c(
-  "luke/04_Metsa/04_Talous/06_Metsateollisuuden_ulkomaankauppa/02_Tuonti_ja_vienti_kuukausittain.px"
-)
-
 id_puunkaytto <-
   c(
-    "luke/04_Metsa/04_Talous/07_Puun_kaytto/08_Metsateollisuuden_puunkaytto/02_metsateol_puunk_toimiala.px",
-    "luke/04_Metsa/04_Talous/07_Puun_kaytto/00_Puun_kok_kaytto.px"
+    "luke/met/puukay/mt/0300_puukay.px",
+    "luke/met/puukay/0100_puukay.px"
   )
 
 id_puukauppa <-
   c(
-    "luke/04_Metsa/04_Talous/02_Teollisuuspuun_kauppa/02_Kuukausitilastot/01a_Kantohinnat_kk.px",
-    "luke/04_Metsa/04_Talous/02_Teollisuuspuun_kauppa/04_Vuositilastot/01b_Kantohinnat_v.px",
-    "luke/04_Metsa/04_Talous/02_Teollisuuspuun_kauppa/02_Kuukausitilastot/03a_Hankintahinnat_kk.px",
-    "luke/04_Metsa/04_Talous/02_Teollisuuspuun_kauppa/04_Vuositilastot/01b_Kantohinnat_v.px",
-    "luke/04_Metsa/04_Talous/16_Kantorahatulot/01a_Bruttokantorahat_v_omistajaryhma_mk.px")
+    "luke/met/teokau/kk/0100_teokau",
+    "luke/met/teokau/v/0400_teokau.px,
+    "luke/met/kanrah/0100_kanrah.px")
 id_puukauppa2 <- c(
-    "luke/04_Metsa/04_Talous/02_Teollisuuspuun_kauppa/02_Kuukausitilastot/07a_Puukauppamaarat_kk_pystykaupat.px",
-    "luke/04_Metsa/04_Talous/02_Teollisuuspuun_kauppa/02_Kuukausitilastot/09a_Puukauppamaarat_kk_hankintakaupat.px",
-    "luke/04_Metsa/02_Rakenne_ja_tuotanto/06_Puun_markkinahakkuut/02_Kuukausitilastot/01_Teollisuuspuun_hakkuut_kk.px",
-    "luke/04_Metsa/02_Rakenne_ja_tuotanto/06_Puun_markkinahakkuut/04_Vuositilastot/03_Teollisuuspuun_hakkuut_v_koko_maa.px"
+    "luke/met/teokau/kk/0100_teokau",
+    "luke/met/teokau/v/0400_teokau.px",
+    "luke/met/marhak/kk/0100_marhak.px",
+    "luke/met/marhak/v/0300_marhak.px"
   )
 
-purrr::map(id_tuotanto_ulkomaank, ~data_to_yaml(data_get(.x), file = "inst/ennustedata/MAdata_Tuotanto_raw.yaml", append = TRUE))
 purrr::map(id_puunkaytto, ~data_to_yaml(data_get(.x), file = "inst/ennustedata/MAdata_puunkaytto_raw.yaml", append = TRUE))
 purrr::map(id_puukauppa2, ~data_to_yaml(data_get(.x), file = "inst/ennustedata/MAdata_puukaupp2_raw.yaml", append = TRUE))
 
-data_get("luke/04_Metsa/04_Talous/06_Metsateollisuuden_ulkomaankauppa/02_Tuonti_ja_vienti_kuukausittain.px") |>
-  data_to_yaml()
 
 ma_tulli_cn8 <- c("44071110", "44071120", "44071190", "44071210", "44071220", "44071290", "44071910", "44071920", "44123310", "44123900", "44124900")
 ma_tulli_cn4 <- c("4407", "4412")
